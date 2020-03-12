b6719992d006b660a426b7792735c87c0ec03f2a
binaks <binaksrc@gmail.com>
2020-03-12 16:59:43 -0300

    plotting but not showing axes

--- T1/q1.py
+++ T1/q1.py
@@ -2,20 +2,29 @@ import PyGnuplot as gp
 from math import *
 import numpy as np
 
-def f(x: int) -> float:
+def f(x):
     return x**3 - 2*x**2 - x + 2
 
-def fprime(x: int) -> float:
+def fprime(x):
     return 3*x**2 - 4*x - 1 
 
 def find_b(a, x, y):
     return y - a*x
+
+def bhaskara(a, b, c):
+    delta = b**2 - 4*a*c
+    root_1 = (-b + sqrt(delta))/(2*a)
+    root_2 = (-b - sqrt(delta))/(2*a)
+    return [root_1, root_2]
 
 def t1q1(f):
     x = 1
 
     gp.c('set terminal eps')
     gp.c('set xrange[-1.5:2.5]')
+    gp.c('set xzeroaxis')
+    gp.c('set yzeroaxis')
+    gp.c('set grid')
     gp.c('set output "t1q1.eps"')
     gp.c('plot x*x*x - 2*x - x + 2 title "função cúbica"')
 
@@ -24,7 +33,18 @@ def t1q1(f):
     b = find_b(a, x, y)
 
     gp.c('set output "t1q1.eps"')
-    gp.c('replot -2*x + 2 title "reta tangente em x = 1"')
+    gp.c(f'replot {a}*x + {b} title "reta tangente em x = 1"')
 
+    roots = bhaskara(3, -4, -1)
+    
+    for root in roots:
+        x = root
+        a = fprime(x)
+        y = f(x)
+        b = find_b(a, x, y)
+
+        gp.c('set output "t1q1.eps"')
+        gp.c(f'replot {a}*x + {b} title "reta tangente em x = {root}"')
+        
 if __name__ == '__main__':
     t1q1(f)

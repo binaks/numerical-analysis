2c5fa3ec806bfd9ed7129c5ad25bcb0006a2bad5
binaks <binaksrc@gmail.com>
2020-03-12 12:53:29 -0300

    Plotting two graphs

--- T1/q1.py
+++ T1/q1.py
@@ -1,7 +1,5 @@
-import sys
 import PyGnuplot as gp
 from math import *
-import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x: int) -> float:
@@ -16,17 +14,17 @@ def find_b(a, x, y):
 def t1q1(f):
     x = 1
 
-    gp.c('set terminal png')
-    gp.c('set output "t1q1.png"')
+    gp.c('set terminal eps')
     gp.c('set xrange[-1.5:2.5]')
-    gp.c('plot x*x*x - 2*x - x + 2 with lines title "função cúbica"')
+    gp.c('set output "t1q1.eps"')
+    gp.c('plot x*x*x - 2*x - x + 2 title "função cúbica"')
 
     a = fprime(x)
     y = f(x)
     b = find_b(a, x, y)
-    
-    gp.c('replot -2*x + 2 with lines title "reta tangente de x em 1"')
+
+    gp.c('set output "t1q1.eps"')
+    gp.c('replot -2*x + 2 title "reta tangente em x = 1"')
 
 if __name__ == '__main__':
-    arg = sys.argv[1] if len(sys.argv) > 2 else None
     t1q1(f)
